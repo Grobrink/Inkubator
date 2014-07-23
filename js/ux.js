0d34433ef4547d7facee38719a431af5922f4b09
Pierre Lefèvre <karapost@gmail.com>
2014-07-23 08:58:26 +0200

    Switch jquery animations to css animations and transitions with js callback

--- js/ux.js
+++ js/ux.js
@@ -1,7 +1,14 @@
 $(function () {
 
 	$(document).on('npcAddedEvent', function() {
+
 		addSlideEvent();
+
+		// Remove the default inactive class
+		// Bug ? If not in a setTimeout (0 ??????) there is no transition
+		setTimeout(function() {
+			$('.stat-block').removeClass('inactive');
+		},10);
 	})
 
 	// $('#inkubator').hammer({threshold: 1}).on("pandown", function(e) {
@@ -35,21 +42,41 @@ $(function () {
 
 			$(this).css({left: pos + 'px', opacity: opacityValue });
 
-			if (Math.abs(pos) > 80) {
-				$(this).remove();
+			if (Math.abs(pos) > 80 && e.gesture.isFinal == 1) {
 
-				if (!$('.stat-block').length) {
-
-					// Generate a NPC at start
-					$(document).trigger('generateNpcEvent');
+				// Element has inline style set by jquery
+				// It doesn't allow opacity transition
+				if ($(this).position().left > 0) {
+					$(this).attr('style', 'left: 100%');
 				}
+				else {
+					$(this).attr('style', 'left: -100%');
+				}
+
+				// Add the inactive class
+				$(this).addClass('inactive');
+
+				// On transition complete remove the element
+				$(this).on('otransitionend transitionend webkitTransitionEnd', function(e) {
+
+					console.log(e);
+
+					// Remove the element
+					$(this).remove();
+
+					// If there is no more element, add a new one
+					if (!$('.stat-block').length) {
+
+						// Generate a NPC at start
+						$(document).trigger('generateNpcEvent');
+					}
+				});
 			}
 
 			// Reset Left position for the last pan event
 			if (e.gesture.isFinal == 1) {
 
 				if (Math.abs(pos) <= 80) {
-					console.log('on replace à zéro');
 					$(this).css({left: '0px', opacity: 1 });
 				}
 			}
